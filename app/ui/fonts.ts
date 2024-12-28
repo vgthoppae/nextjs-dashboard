fbcbb51781dc868f1fd21a94b0de4f9f8d65b2c4
vgthoppae <vgthoppae@gmail.com>
2024-12-28 17:44:47 -0500

    tutorial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Funnel_Sans } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '400' });
+export const funnel_sans = Funnel_Sans({ weight: '400' });
