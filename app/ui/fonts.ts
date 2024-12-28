679a9ac5edfbb7bfd60a3e8664480520f3d3e031
vgthoppae <vgthoppae@gmail.com>
2024-12-28 17:48:29 -0500

    add subsets

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
 import { Inter, Lusitana, Funnel_Sans } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({ weight: '400' });
-export const funnel_sans = Funnel_Sans({ weight: '400' });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
+export const funnel_sans = Funnel_Sans({ subsets: ['latin'], weight: '400' });
